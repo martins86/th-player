aaba7b8d6491af62e33ceb1bcc0e52ad86725e9a
Thiago Martins de Freitas <46196327+martins86@users.noreply.github.com>
2026-05-30 00:09:12 +0000

    Add code coverage reports and improve code formatting
    - Added lcov.info for code coverage data of src/App.tsx and src/main.tsx.
    - Created HTML report for main.tsx coverage with detailed statistics.
    - Included CSS and JS files for styling and functionality of the coverage report.
    - Added a sort arrow sprite image for sorting indicators in the coverage report.
    - Improved the sorting functionality in the coverage report.
    - Refactored App.tsx and index.css for better code formatting and consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
         <p>Base limpa para iniciar o novo app IPTV para LG webOS.</p>
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
