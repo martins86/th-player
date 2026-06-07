8a3c4a908e9ab704f8a28e0f862375744770f956
Thiago Martins de Freitas <46196327+martins86@users.noreply.github.com>
2026-06-07 18:14:35 +0000

    [THP-001] FEAT - config lint, husky e vitest

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist', 'node_modules', 'coverage']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      tseslint.configs.recommended,
+    ],
+  },
+])
