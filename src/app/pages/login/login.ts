b0a8f530fb20663c88fb92c71b90cbd156c619fa
Thiago Martins de Freitas <46196327+martins86@users.noreply.github.com>
2026-06-11 22:10:17 +0000

    [THP-001] FEAT - iniciando login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
-import { Header } from '@shared/components/header/header';
-import { Footer } from '@shared/components/footer/footer';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ILogin } from '@shared/interfaces/login.interface';
 
 @Component({
   selector: 'app-login',
-  imports: [Header, Footer],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  loginForm!: FormGroup<ILogin>;
+
+  constructor() {
+    this.loadForm();
+  }
+
+  loadForm(): void {
+    this.loginForm = new FormGroup({
+      link: new FormControl('', [Validators.required]),
+      user: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+}
---
+++ src/app/shared/interfaces/login.interface.ts
@@ -0,0 +1,7 @@
+import { FormControl } from '@angular/forms';
+
+export interface ILogin {
+  link: FormControl;
+  user: FormControl;
+  password: FormControl;
+}
